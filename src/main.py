74d7c6ae24167bf6c2b8aaa5687eb60807ea7ae4
HicaroD <hdanrlley1@gmail.com>
2021-09-26 18:21:29 -0300

    Creating an add_input_button in the Button class

--- src/main.py
+++ src/main.py
@@ -1,28 +1,37 @@
 import tkinter
-from project_manager import ProjectManager
+from project_manager import ProjectManager, Widget
 
 """
 TODO:
     [X] FEATURE: Add a way to make the user insert a name for the project and
                  create a new one (without passing a parameter for the name)
 
+    [] FEATURE: Add a button for inserting a input
+
+    [] FEATURE: Insert a current datetime for each note input in the project
+
     [] FEATURE: Add a way to read text file using Tkinter
 
-    [] FEATURE: Add a better way to handle / manage the button creation (or leave it like that)
+    [X] FEATURE: Add a better way to handle / manage the button creation (or leave it like that)
 """
 
 
 class ButtonManager():
-    def __init__(self, master : tkinter.Tk):
-        self.master = master
+    def __init__(self, window : tkinter.Tk):
+        self.window = window
         self.project_manager = ProjectManager()
 
     def create_button(self, text : str, command):
-        return tkinter.Button(self.master, text = text, command = command)
+        return tkinter.Button(self.window, text = text, command = command, width=12, height=5)
 
     def add_project_button(self):
         add_proj_bttn = self.create_button("Add project", self.project_manager.create_project)
         add_proj_bttn.pack(side=tkinter.constants.LEFT, anchor=tkinter.constants.NW)
+
+    def add_input_button(self):
+        # TODO: Create add_input method and insert in the button bellow
+        add_input_btton = self.create_button("Add a new input")
+        add_input_btton.pack(side=tkinter.constants.LEFT, anchor=tkinter.constants.NW)
 
 
 class Application(tkinter.Frame):
@@ -30,6 +39,7 @@ class Application(tkinter.Frame):
         super().__init__(master)
         self.master = master
         self.buttons = ButtonManager(self.master)
+        self.widget = Widget(self.master)
         self.configure_gui()
 
     def configure_gui(self):
@@ -37,6 +47,8 @@ class Application(tkinter.Frame):
         self.master.geometry("720x480")
         self.master.resizable(False, False)
         self.buttons.add_project_button()
+        self.buttons.add_input_button()
+        self.widget.create_project_list_box()
 
 
 def main():
