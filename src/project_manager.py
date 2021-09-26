c2dda88df634fca743a3b93d9c2b4dd7171ff64b
HicaroD <hdanrlley1@gmail.com>
2021-09-26 18:22:13 -0300

    Creating an Widget class to build an ListBox
    ListBox is gonna be used to list the project notes folder

--- src/project_manager.py
+++ src/project_manager.py
@@ -1,20 +1,20 @@
-import tkinter
 from tkinter import messagebox, simpledialog
+from datetime import datetime
+from tkinter import ttk
+import tkinter
 import os
 
 CURRENT_SCRIPT_FILE_PATH = os.path.dirname(os.path.abspath(__file__))
+PROJECT_NOTES_FOLDER_PATH = os.path.join(CURRENT_SCRIPT_FILE_PATH, "project_notes/")
 
 class ProjectManager:
-    def __init__(self):
-        self.project_notes_folder_path = os.path.join(CURRENT_SCRIPT_FILE_PATH, "project_notes/")
-
     def ask_for_project_name(self):
         return tkinter.simpledialog.askstring("What is the name of project?", "Insert the name of the project")
 
     def create_file(self, project_name, path_for_project_notes):
         """Creates txt file to store the notes"""
         with open(path_for_project_notes, 'x') as f:
-            f.write(project_name.title())
+            f.write(project_name.title() + "\n")
 
     def create_project(self):
         try:
@@ -22,7 +22,7 @@ class ProjectManager:
 
             if project_name is not None:
                 file_name_for_project = f"{project_name}.txt".replace(" ", "_")
-                path_for_project_notes = os.path.join(self.project_notes_folder_path, file_name_for_project)
+                path_for_project_notes = os.path.join(PROJECT_NOTES_FOLDER_PATH, file_name_for_project)
 
                 self.create_file(project_name, path_for_project_notes)
 
@@ -32,3 +32,20 @@ class ProjectManager:
             if(want_to_overwrite):
                 os.remove(path_for_project_notes)
                 self.create_file(project_name, path_for_project_notes)
+
+    def add_input_note(self):
+        current_time = datetime.now()
+        input_note = None
+
+class Widget:
+    def __init__(self, master : tkinter.Tk):                                                                                               
+       self.master = master                                                                                                               
+
+    def create_project_list_box(self):                                                                 
+        projects = [project.rstrip(".txt").title() for project in os.listdir(PROJECT_NOTES_FOLDER_PATH)]     
+        list_box = tkinter.Listbox(self.master, height=10, width=15, activestyle='dotbox')
+        
+        for project_index, project in enumerate(projects):
+            list_box.insert(project_index, project)
+
+        list_box.pack(side=tkinter.constants.TOP, anchor=tkinter.constants.NE)
