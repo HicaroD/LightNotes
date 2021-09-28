0c3e122625b58007d90bb2ca3de9f6b948cbc695
HicaroD <hdanrlley1@gmail.com>
2021-09-27 23:07:32 -0300

    Creating an add_input_note method for adding new notes with timestamp

--- src/project_manager.py
+++ src/project_manager.py
@@ -1,24 +1,22 @@
-from tkinter import messagebox, simpledialog
+from tkinter import messagebox, simpledialog, filedialog
 from datetime import datetime
 from tkinter import ttk
 import tkinter
 import os
 
-CURRENT_SCRIPT_FILE_PATH = os.path.dirname(os.path.abspath(__file__))
-PROJECT_NOTES_FOLDER_PATH = os.path.join(CURRENT_SCRIPT_FILE_PATH, "project_notes/")
+CURRENT_SCRIPT_FILE_ABSOLUTE_PATH = os.path.dirname(os.path.abspath(__file__))
+PROJECT_NOTES_FOLDER_PATH = os.path.join(CURRENT_SCRIPT_FILE_ABSOLUTE_PATH, "project_notes/")
 
 class ProjectManager:
-    def ask_for_project_name(self):
-        return tkinter.simpledialog.askstring("What is the name of project?", "Insert the name of the project")
-
     def create_file(self, project_name, path_for_project_notes):
         """Creates txt file to store the notes"""
         with open(path_for_project_notes, 'x') as f:
-            f.write(project_name.title() + "\n")
+            f.write(project_name.title() + "\n\n")
 
     def create_project(self):
+        """Creates a project in the project_notes folder"""
         try:
-            project_name = self.ask_for_project_name()
+            project_name = Widget.ask_for_project_name()
 
             if project_name is not None:
                 file_name_for_project = f"{project_name}.txt".replace(" ", "_")
@@ -27,25 +25,41 @@ class ProjectManager:
                 self.create_file(project_name, path_for_project_notes)
 
         except FileExistsError:
-            want_to_overwrite = messagebox.askyesno("WARNING", "Project already exists! \nDo you want overwrite it?")
+            wants_to_overwrite_project = messagebox.askyesno("WARNING", "Project already exists! \nDo you want overwrite it?")
 
-            if(want_to_overwrite):
+            if(wants_to_overwrite_project):
                 os.remove(path_for_project_notes)
                 self.create_file(project_name, path_for_project_notes)
 
     def add_input_note(self):
-        current_time = datetime.now()
-        input_note = None
+        """Add a single note to an existing project"""
+        try:
+            current_time = datetime.now()
+            project_note_full_path = Widget.get_project_note_full_path()
+
+            if project_note_full_path is not None or project_note_full_path != ():
+                input_note = Widget.get_input_note("Input", "Add note to project in the dialog below:")
+                print(input_note)
+
+                if(input_note is not None):
+                    with open(project_note_full_path, 'a', encoding="utf-8") as project_note:
+                        date_time = current_time.strftime("\U0001F4C5 %m/%d/%Y || \U0001F551 %H:%M:%S\n")
+                        project_note.write(date_time)
+                        project_note.write(input_note + "\n\n")
+
+        except Exception as e:
+            # TODO: Create a better error handling soon
+            print(e)
 
 class Widget:
-    def __init__(self, master : tkinter.Tk):                                                                                               
-       self.master = master                                                                                                               
+    @staticmethod
+    def ask_for_project_name():
+        return tkinter.simpledialog.askstring("What is the name of project?", "Insert the name of the project")
 
-    def create_project_list_box(self):                                                                 
-        projects = [project.rstrip(".txt").title() for project in os.listdir(PROJECT_NOTES_FOLDER_PATH)]     
-        list_box = tkinter.Listbox(self.master, height=10, width=15, activestyle='dotbox')
-        
-        for project_index, project in enumerate(projects):
-            list_box.insert(project_index, project)
+    @staticmethod
+    def get_project_note_full_path():
+        return tkinter.filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
 
-        list_box.pack(side=tkinter.constants.TOP, anchor=tkinter.constants.NE)
+    @staticmethod
+    def get_input_note(dialog_title : str, dialog_label : str):
+        return tkinter.simpledialog.askstring(dialog_title, dialog_label)
