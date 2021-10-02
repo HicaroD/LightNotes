79b8c585a5f9bfd239531a447965c973667b8b3a
HicaroD <hdanrlley1@gmail.com>
2021-10-02 16:41:36 -0300

    see_notes_button to draw text_widget on the screen
    Additionally, I created a dict for storing the side and anchor of
    top-left corner for positioning the buttons on the screen

--- src/main.py
+++ src/main.py
@@ -8,39 +8,49 @@ TODO:
 
     [X] FEATURE: Add a "info" button in the bottom right corner to open the default
                 browser of the user in the repository of LightNotes
+
     [] BUG: When I insert a new entry in any project note, the changes don't show up immediately, I need to
             to insert another to make previous changes appear
-            
 """
 
-WINDOW_THEME = "flatly"
+TOP_LEFT_CORNER_POSITIONS = {"side" : tkinter.constants.LEFT, "anchor" : tkinter.constants.NW}
 
 class ButtonManager():
     def __init__(self, window : tkinter.Tk):
         self.window = window
-        self.project_manager = ProjectManager()
+        self.project_manager = ProjectManager(window)
 
     def draw_buttons(self):
         self.add_project_button()
         self.add_input_button()
         self.add_info_button()
+        self.see_note_button()
 
     def create_button(self, text : str, command, style = "primary.TButton"):
         return tkinter.ttk.Button(self.window, text = text, style = style, command = command)
 
     def add_project_button(self):
         add_proj_bttn = self.create_button("Add project", self.project_manager.create_project)
-        add_proj_bttn.pack(side = tkinter.constants.LEFT, anchor = tkinter.constants.NW)
+        add_proj_bttn.pack(side = TOP_LEFT_CORNER_POSITIONS["side"], 
+                           anchor = TOP_LEFT_CORNER_POSITIONS["anchor"])
 
     def add_input_button(self):
-        add_input_btton = self.create_button("Add a new input", lambda: self.project_manager.add_input_note(self.window))
-        add_input_btton.pack(side = tkinter.constants.LEFT, anchor = tkinter.constants.NW)
+        add_input_btton = self.create_button("Add a new input", self.project_manager.add_input_note)
+        add_input_btton.pack(side = TOP_LEFT_CORNER_POSITIONS["side"], 
+                             anchor = TOP_LEFT_CORNER_POSITIONS["anchor"])
 
     def add_info_button(self):
         from project_manager import info
         add_info_bttn = self.create_button("Info", style = "info.TButton", command = info)
         add_info_bttn.pack(side = tkinter.constants.RIGHT, anchor = tkinter.constants.SE)
+    
+    def see_note_button(self):
+        see_note_bttn = self.create_button("See notes", self.project_manager.see_notes)
+        see_note_bttn.pack(side = TOP_LEFT_CORNER_POSITIONS["side"], 
+                           anchor = TOP_LEFT_CORNER_POSITIONS["anchor"])
 
+
+WINDOW_THEME = "journal"
 
 class Application(tkinter.Frame):
     def __init__(self, master):
