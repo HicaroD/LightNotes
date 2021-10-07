78488ae5ecd81177fbd46b3282e16d2d36521cfd
HicaroD <hdanrlley1@gmail.com>
2021-10-07 16:49:16 -0300

    Separating widgets implementation in a separated file

---
+++ src/widgets.py
@@ -0,0 +1,18 @@
+from tkinter import simpledialog, filedialog, messagebox
+
+class Widget:
+    def ask_for_project_name(self):
+        return simpledialog.askstring("Project name", "Insert the name of the project")
+
+    def get_project_note_full_path(self):
+        return filedialog.askopenfilename(initialdir = PROJECT_NOTES_FOLDER_PATH,
+                                                  filetypes=[("Text files", "*.txt")])
+
+    def get_input_note(self):
+        return simpledialog.askstring("Input", "Add note to project in the dialog below:")
+
+    def wants_to_overwrite_project(self) -> bool:
+        return messagebox.askyesno("WARNING", "Project already exists! \nDo you want overwrite it?")
+
+    def show_warning_for_empty_project_notes(self):
+        messagebox.showwarning("Folder is empty", "You should create a project first")
