fd67ff48cabd72df7032eedd16be359177232cca
HicaroD <hdanrlley1@gmail.com>
2021-10-07 16:49:41 -0300

    Deleting Widget class to insert into another file

--- src/project_manager.py
+++ src/project_manager.py
@@ -3,7 +3,8 @@ import tkinter
 import webbrowser
 from datetime import datetime
 from tkinter import ttk
-from tkinter import messagebox, simpledialog, filedialog
+from tkinter import messagebox
+from widgets import Widget
 
 CURRENT_MAIN_FILE_ABSOLUTE_PATH = os.path.dirname(os.path.abspath(__file__))
 PROJECT_NOTES_FOLDER_PATH = os.path.join(CURRENT_MAIN_FILE_ABSOLUTE_PATH, "project_notes/")
@@ -117,21 +118,3 @@ class ProjectManager:
 
         except (TypeError, FileNotFoundError) as e:
             messagebox.showwarning("Invalid input", "Select a valid file")
-
-class Widget:
-    def ask_for_project_name(self):
-        return tkinter.simpledialog.askstring("Project name", "Insert the name of the project")
-
-    def get_project_note_full_path(self):
-        return tkinter.filedialog.askopenfilename(initialdir = PROJECT_NOTES_FOLDER_PATH,
-                                                  filetypes=[("Text files", "*.txt")])
-
-    def get_input_note(self):
-        return tkinter.simpledialog.askstring("Input", "Add note to project in the dialog below:")
-
-    def wants_to_overwrite_project(self) -> bool:
-        return messagebox.askyesno("WARNING", "Project already exists! \nDo you want overwrite it?")
-
-    def show_warning_for_empty_project_notes(self):
-        messagebox.showwarning("Folder is empty", "You should create a project first")
-        return
